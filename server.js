f191450adad2cdaa25d001adaca8e7a17a990cd3
stas117228-cmyk <stas117228@gmail.com>
2025-11-01 16:39:23 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+﻿import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log(✅ Server running on port ));
